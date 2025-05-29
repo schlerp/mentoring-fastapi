109a6c29a6040cb8f774620b3ee1d279ff50a7af
PattyC <patrickcoffey91@gmail.com>
2025-05-29 13:55:55 +0930

    Update the `EchoResponse` model to use new basemodel
    Prior to this change, there was no common base model to enable
    configurations of all schemas across the application.
    
    This change adds a new base class and swaps the `EchoResponse` schema to
    instead use this base model rather than directly inherit from the
    pydantic base model.

--- src/mentoring_fastapi/schemas.py
+++ src/mentoring_fastapi/schemas.py
@@ -1,5 +1,20 @@
+import datetime
+import typing
+
 import pydantic
 
 
-class EchoResponse(pydantic.BaseModel):
+class OrmBaseModel(pydantic.BaseModel):
+    """
+    Base model for ORM objects to enable ORM mode.
+
+    NOTE: ORM mode has been renamed to `from_attributes` in Pydantic v2.
+    """
+
+    model_config: typing.ClassVar[pydantic.ConfigDict] = pydantic.ConfigDict(
+        from_attributes=True,
+    )
+
+
+class EchoResponse(OrmBaseModel):
     greeting: str
